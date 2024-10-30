95b62072ab7e3304a5e8299d2492206a6c473718
Francis7575 <francis.albert.2009@gmail.com>
2024-10-29 22:21:28 -0700

    Installed clerk and integrate middleware.ts

---
+++ src/app/provider.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { ReactNode } from 'react'
+
+interface ProviderProps {
+  children: ReactNode
+}
+
+const Provider = ({children}: ProviderProps) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Provider
