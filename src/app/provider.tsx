64f40d3b36910c4c2d70e17acb74e2a87639f36e
Francis7575 <francis.albert.2009@gmail.com>
2024-10-30 17:55:02 -0700

    utilize useUser hook from clerk/nextjs to access the user information

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,16 +1,40 @@
 "use client"
-import { ReactNode } from 'react'
+import { useUser } from '@clerk/nextjs'
+import { ReactNode, useEffect } from 'react'
 
 interface ProviderProps {
-  children: ReactNode
+	children: ReactNode
 }
 
-const Provider = ({children}: ProviderProps) => {
-  return (
-    <div>
-      {children}
-    </div>
-  )
+const Provider = ({ children }: ProviderProps) => {
+const { user } = useUser();
+
+	useEffect(() => {
+		if (user) verifyUser();
+	}, [user])
+	
+	const verifyUser = async () => {
+		const response = await fetch('/api/verify-user', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ user: user })
+		})
+		
+		if (!response.ok) {
+			throw new Error('Failed to verify user');
+		}
+	
+		const data = await response.json();
+		console.log(data)
+		return data
+	}
+	return (
+		<div>
+			{children}
+		</div>
+	)
 }
 
 export default Provider
