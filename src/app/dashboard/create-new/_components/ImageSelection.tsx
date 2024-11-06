1c904e17f17b7cc7dbbb01ad53a626dcb04fbc2a
Francis7575 <francis.albert.2009@gmail.com>
2024-11-06 12:49:23 -0800

    installed browser-image-compression package

--- src/app/dashboard/create-new/_components/ImageSelection.tsx
+++ src/app/dashboard/create-new/_components/ImageSelection.tsx
@@ -1,4 +1,5 @@
 'use client'
+import imageCompression from 'browser-image-compression';
 import Image from "next/image"
 import { ChangeEvent, useState } from "react"
 
@@ -6,14 +7,20 @@ type ImageSelectionProps = {
   selectedImage: (value: File) => void
 }
 
-const ImageSelection = ({selectedImage}: ImageSelectionProps) => {
-const [file, setFile] = useState<File | null>(null)
+const ImageSelection = ({ selectedImage }: ImageSelectionProps) => {
+  const [file, setFile] = useState<File | null>(null)
 
-  const onFileSelected = (e: ChangeEvent<HTMLInputElement>) => {
+  const onFileSelected = async (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       const selectedFile = e.target.files[0];
-      setFile(selectedFile);
-      selectedImage(selectedFile); 
+
+      const compressedFile = await imageCompression(selectedFile, {
+        maxSizeMB: 1, 
+        maxWidthOrHeight: 1024, 
+      });
+
+      setFile(compressedFile);
+      selectedImage(compressedFile);
     }
   }
 
@@ -24,11 +31,11 @@ const [file, setFile] = useState<File | null>(null)
         <label htmlFor="upload-image">
           <div className={`flex justify-center border-primary  bg-slate-200 cursor-pointer 
               border rounded-xl border-dotted hover:shadow-lg ${file ? 'p-0 bg-white' : 'p-28'}`}>
-                {!file ? (
-                  <Image src={'/image-upload.png'} width={70} height={70} alt="" />
-                ) : (
-                  <Image src={URL.createObjectURL(file)} width={300} height={300} alt="" className="object-fill h-auto w-auto"/>
-                )}
+            {!file ? (
+              <Image src={'/image-upload.png'} width={70} height={70} alt="" />
+            ) : (
+              <Image src={URL.createObjectURL(file)} width={300} height={300} alt="" className="object-fill h-auto w-auto" />
+            )}
           </div>
         </label>
         <input className="hidden"
